bb2aa75353a9f8b62cbb62c99062c364be26f23a
ACortesPalomares <cortes.palomares.angello00@gmail.com>
2026-04-10 22:45:14 -0500

    Menu Principal funcional sin sprites

--- ProyectoCG/Assets/Game/Scripts/ControllerMenu.cs
+++ ProyectoCG/Assets/Game/Scripts/ControllerMenu.cs
@@ -4,6 +4,10 @@ public class ControllerMenu : MonoBehaviour
 {
 
     public GameObject continueButton;
+    public GameObject settingsPanel;
+    public GameObject videoConfigPanel;
+    public GameObject audioConfigPanel;
+    public GameObject controlsConfigPanel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +18,37 @@ public class ControllerMenu : MonoBehaviour
     void Update()
     {
 
+    }
+
+    public void OpenVideoConfig()
+    {
+        videoConfigPanel.SetActive(true);
+        audioConfigPanel.SetActive(false);
+        controlsConfigPanel.SetActive(false);
+    }
+
+    public void OpenAudioConfig()
+    {
+        videoConfigPanel.SetActive(false);
+        audioConfigPanel.SetActive(true);
+        controlsConfigPanel.SetActive(false);
+    }
+
+    public void OpenControlsConfig()
+    {
+        videoConfigPanel.SetActive(false);
+        audioConfigPanel.SetActive(false);
+        controlsConfigPanel.SetActive(true);
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
     }
 
     public void StartGame()
